17d2490aed9d5e8f673df069864ff0542b26a8c7
Rishit <rishpandey8097@gmail.com>
2023-11-09 01:54:58 +0530

    add: sentry

---
+++ src/core/sentry.ts
@@ -0,0 +1,23 @@
+import * as Sentry from '@sentry/node';
+import { RewriteFrames } from '@sentry/integrations';
+import config from './config';
+
+Sentry.init({
+  dsn: config.sentry.dsn,
+  environment: config.sentry.environment,
+  debug: true,
+  attachStacktrace: true,
+  normalizeDepth: 5,
+  includeLocalVariables: true,
+  integrations: [new RewriteFrames({ root: global.__dirname })],
+});
+
+export const captureMessage = (message: string, payload: object) => {
+  for (const entry in Object.entries(payload)) Sentry.setExtra(entry[0], entry[1]);
+  Sentry.captureMessage(message);
+};
+
+export const captureException = (exception: Error, payload: object) => {
+  for (const entry in Object.entries(payload)) Sentry.setExtra(entry[0], entry[1]);
+  Sentry.captureException(exception);
+};
